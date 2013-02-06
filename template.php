679023e95cdeb8349adda6aca9184a4f02820dff
Aegir <aegir@local>
2013-02-06 15:51:45 +0700

    fixed search submit

--- template.php
+++ template.php
@@ -424,6 +424,34 @@ function genesis_btfp_file_icon($variables) {
   return '<img class="file-icon" alt="' . $mime . '" title="' . $mime . '" src="' . $icon_url . '" />';
 }
 
+function genesis_btfp_file_link($variables) {
+  $file = $variables['file'];
+  $icon_directory = $variables['icon_directory'];
+
+  $url = file_create_url($file->uri);
+  $icon = theme('file_icon', array('file' => $file, 'icon_directory' => $icon_directory));
+
+  // Set options as per anchor format described at
+  // http://microformats.org/wiki/file-format-examples
+  $options = array(
+    'attributes' => array(
+      'type' => $file->filemime . '; length=' . $file->filesize,
+      'target' => '_blank',
+    ),
+  );
+
+  // Use the description as the link text if available.
+  if (empty($file->description)) {
+    $link_text = $file->filename;
+  }
+  else {
+    $link_text = $file->description;
+    $options['attributes']['title'] = check_plain($file->filename);
+  }
+
+  return '<span class="file">' . $icon . ' ' . l($link_text, $url, $options) . '</span>';
+}
+
 function genesis_btfp_form_element($variables) {
   $element = &$variables['element'];
   // This is also used in the installer, pre-database setup.
